aa8a63181279f533e14c24052e688119efc33d2c
Hồng Liên <hlien2k2@gmail.com>
2021-08-17 18:37:33 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Hey Bae!!!',
+    introDesc: `
+    Em có thể hỏi anh vài câu được không? Trả lời thật lòng nhé 🥺`,
+    btnIntro: 'Okay ❤',
+    title: 'Có thể anh không phải chàng trai đặc biệt giữa đám đông. Nhưng anh là chàng trai đặc biệt trong lòng em!',
+    desc: 'Thế giới của em bừng sáng khi anh xuất hiện. Làm người yêu em nhé?',
+    btnYes: 'Okay luôn em êu <33',
+    btnNo: 'No nhé cô gái :3',
+    question:'Trên thế giới có hơn 8 tỷ người...sao anh lại chọn em?',
+    btnReply: 'Gữi cho em 😋',
+    reply: 'Yêu thì yêu mà không yêu cũng...yêu <33333333',
+    mess: 'Em biết mà 🥰. I love you 9999',
+    messDesc: 'Inbox với em tý nhé',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/100045977767715'
+}
